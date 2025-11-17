c9e9113be86188e158edbcf7f06071c16b3cab9b
amilearning <hojin.projects@gmail.com>
2025-11-17 21:00:26 +0100

    init hierarchical

--- rsl_rl/runners/hierarchical_runner.py
+++ rsl_rl/runners/hierarchical_runner.py
@@ -12,20 +12,21 @@ from collections import deque
 from tensordict import TensorDict
 
 import rsl_rl
-from rsl_rl.algorithms import Cloning
+from rsl_rl.algorithms import Distillation
 from rsl_rl.env import VecEnv
-from rsl_rl.modules import BCStudentTeacher
+from rsl_rl.modules import StudentTeacher, StudentTeacherRecurrent
 from rsl_rl.runners import OnPolicyRunner
 from rsl_rl.utils import resolve_obs_groups, store_code_state
 
 
-class ClonPolicyRunner(OnPolicyRunner):
-    """On-policy runner for training and evaluation of teacher-student training."""
+class HierarchicalRunner(OnPolicyRunner):
+    """On-policy runner for training and evaluation of high and low level hierarchical training."""
 
     def __init__(self, env: VecEnv, train_cfg: dict, log_dir: str | None = None, device: str = "cpu") -> None:
         self.cfg = train_cfg
         self.alg_cfg = train_cfg["algorithm"]
-        self.policy_cfg = train_cfg["policy"]
+        self.low_policy_cfg = train_cfg["low_policy"]
+        self.high_policy_cfg = train_cfg["high_policy"]
         self.device = device
         self.env = env
 
@@ -38,7 +39,7 @@ class ClonPolicyRunner(OnPolicyRunner):
 
         # Query observations from environment for algorithm construction
         obs = self.env.get_observations()
-        self.cfg["obs_groups"] = resolve_obs_groups(obs, self.cfg["obs_groups"], default_sets=["policy", "teacher","previliege"])
+        self.cfg["obs_groups"] = resolve_obs_groups(obs, self.cfg["obs_groups"], default_sets=["teacher"])
 
         # Create the algorithm
         self.alg = self._construct_algorithm(obs)
@@ -87,33 +88,21 @@ class ClonPolicyRunner(OnPolicyRunner):
         # Start training
         start_iter = self.current_learning_iteration
         tot_iter = start_iter + num_learning_iterations
-        
-        self.save(os.path.join(self.log_dir, f"model_prior_train.pt"))
-
         for it in range(start_iter, tot_iter):
-            
             start = time.time()
-            ## 
-            
             # Rollout
             with torch.inference_mode():
-                self.env.reset()
-                prev_contact = obs["previliege"].clone()
-                contact_changed_any = torch.tensor(False,device=self.device)
-                init_count = 0
-                init_count_max = 50
-                while init_count < init_count_max:                    
-                    if (obs['previliege'].float().mean() >= 0.9):
-                        break               
-                    actions = self.alg.rollout(obs)
-                    obs, rewards, dones, extras = self.env.step(actions.to(self.env.device))                    
-                    init_count+=1
-
-                for rollout_step in range(self.num_steps_per_env):
-                    # Sample actions
-                    actions = self.alg.rollout(obs)
+                for _ in range(self.num_steps_per_env):
+                    
+                    # Sample low-level actions
+                    actions = self.alg.act(obs)
+                    ''' 
+                    low-level mdp                    
+                    '''
                     # Step the environment
                     obs, rewards, dones, extras = self.env.step(actions.to(self.env.device))
+
+                    
                     # Move to device
                     obs, rewards, dones = (obs.to(self.device), rewards.to(self.device), dones.to(self.device))
                     # Process the step
@@ -134,22 +123,13 @@ class ClonPolicyRunner(OnPolicyRunner):
                         lenbuffer.extend(cur_episode_length[new_ids][:, 0].cpu().numpy().tolist())
                         cur_reward_sum[new_ids] = 0
                         cur_episode_length[new_ids] = 0
-                    
-                    if contact_changed_any.item() is False: 
-                        contact_changed_any = (obs["previliege"] !=prev_contact).any()                    
-                    prev_contact = obs["previliege"].clone()
 
-            stop = time.time()
-            collection_time = stop - start
-            start = stop
-            
-            # self.alg.relabeling_batch( env_cfg = self.env.cfg)
+                stop = time.time()
+                collection_time = stop - start
+                start = stop
 
-           
-            if contact_changed_any:                
-                loss_dict = self.alg.update()
-            else:
-                print(f"No contact change found... consider increase currentnum_steps_per_env {self.num_steps_per_env}...")
+            # Update policy
+            loss_dict = self.alg.update()
 
             stop = time.time()
             learn_time = stop - start
@@ -177,17 +157,17 @@ class ClonPolicyRunner(OnPolicyRunner):
         if self.log_dir is not None and not self.disable_logs:
             self.save(os.path.join(self.log_dir, f"model_{self.current_learning_iteration}.pt"))
 
-    def _construct_algorithm(self, obs: TensorDict) -> Cloning:
+    def _construct_algorithm(self, obs: TensorDict) -> Distillation:
         """Construct the distillation algorithm."""
         # Initialize the policy
         student_teacher_class = eval(self.policy_cfg.pop("class_name"))
-        student_teacher: BCStudentTeacher  = student_teacher_class(
+        student_teacher: StudentTeacher | StudentTeacherRecurrent = student_teacher_class(
             obs, self.cfg["obs_groups"], self.env.num_actions, **self.policy_cfg
         ).to(self.device)
 
         # Initialize the algorithm
         alg_class = eval(self.alg_cfg.pop("class_name"))
-        alg: Cloning = alg_class(
+        alg: Distillation = alg_class(
             student_teacher, device=self.device, **self.alg_cfg, multi_gpu_cfg=self.multi_gpu_cfg
         )
 
@@ -201,79 +181,3 @@ class ClonPolicyRunner(OnPolicyRunner):
         )
 
         return alg
-
-    def get_inference_policy(self, device: str | None = None) -> callable:
-        self.eval_mode()  # Switch to evaluation mode (e.g. for dropout)
-        if device is not None:
-            self.alg.policy.to(device)
-        return self.alg.policy.act_inference
-    
-
-    def load(self, path: str, load_optimizer: bool = True, map_location: str | None = None) -> dict:
-        loaded_dict = torch.load(path, weights_only=False, map_location=map_location)
-        # Load model
-        resumed_training = self.alg.policy.load_state_dict(loaded_dict["model_state_dict"])
-        # Load optimizer if used
-        if load_optimizer and resumed_training:
-            # Algorithm optimizer
-            self.alg.optimizer.load_state_dict(loaded_dict["optimizer_state_dict"])
-        # Load current learning iteration
-        if resumed_training:
-            self.current_learning_iteration = loaded_dict["iter"]
-        return loaded_dict["infos"]
-    
-
-
-    def teacher_eval(self, num_eval_iterations: int) -> None:
-        self._prepare_logging_writer()
-        obs = self.env.get_observations().to(self.device)
-        self.eval_mode()  
-        start_iter = 0
-        tot_iter = num_eval_iterations                
-        
-        for it in range(start_iter, tot_iter): 
-            pose_error_w = torch.zeros(self.num_steps_per_env, self.env.num_envs, dtype=torch.float, device=self.device)           
-            
-            with torch.inference_mode():
-                for rollout_step in range(self.num_steps_per_env):
-                    actions = self.alg.rollout(obs)
-                    obs, rewards, dones, extras = self.env.step(actions.to(self.env.device))                    
-                    if self.log_dir is not None:
-                        pose_error_w[rollout_step,:] =  obs['evaluation'].squeeze().clone()
-                       
-            self.writer.add_scalar("Eval_teacher/pose_error_w_mean", pose_error_w.mean().cpu().numpy().tolist(), it)
-            self.writer.add_scalar("Eval_teacher/pose_error_w_std", pose_error_w.std().cpu().numpy().tolist(), it)
-
-     
-     
-    def student_eval(self, num_eval_iterations: int) -> None:      
-        self._prepare_logging_writer()
-        obs = self.env.get_observations().to(self.device)
-        self.eval_mode()  # switch to train mode (for dropout for example)
-        student_policy = self.get_inference_policy(device=self.env.device)
-        start_iter = 0
-        tot_iter = num_eval_iterations                
-        
-        for it in range(start_iter, tot_iter): 
-            pose_error_w = torch.zeros(self.num_steps_per_env, self.env.num_envs, dtype=torch.float, device=self.device)           
-            with torch.inference_mode():
-                self.env.reset()
-                init_count = 0
-                init_count_max = 50
-                while init_count < init_count_max:                    
-                    if (obs['previliege'].float().mean() >= 0.9):
-                        break               
-                    teacher_actions = self.alg.rollout(obs)
-                    obs, rewards, dones, extras = self.env.step(teacher_actions.to(self.env.device))                    
-                    init_count+=1
-
-
-                for rollout_step in range(self.num_steps_per_env):   
-                    actions = student_policy(obs)
-                    obs, rewards, dones, extras = self.env.step(actions.to(self.env.device))                
-                    if self.log_dir is not None:
-                        pose_error_w[rollout_step,] =  obs['evaluation'].squeeze().clone()
-                       
-            self.writer.add_scalar("Eval_student/pose_error_w_mean", pose_error_w.mean().cpu().numpy().tolist(), it)
-            self.writer.add_scalar("Eval_student/pose_error_w_std", pose_error_w.std().cpu().numpy().tolist(), it)
-
